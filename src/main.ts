08369964c252d38f080f38aea0c72b793c5baf64
joshub <jos.ct101@gmail.com>
2022-10-26 18:04:45 -0500

    Adding events to manage styles
    Signed-off-by: joshub <jos.ct101@gmail.com>

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,20 @@
 import "./style.css";
 import data from "./data.json";
 
-console.log(data);
+const $time = document.querySelectorAll(".time") as NodeListOf<HTMLDivElement>;
+
+/**### HOVER ### */
+const hoverElement = (data: NodeListOf<HTMLDivElement>) => {
+	data.forEach((item) => {
+		item.addEventListener("mouseover", () => {
+			item.parentElement!.classList.add("work-bg");
+		});
+
+		item.addEventListener("mouseleave", () => {
+			item.parentElement!.classList.remove("work-bg");
+		});
+	});
+};
+
+hoverElement($time);
+//console.log(data);
